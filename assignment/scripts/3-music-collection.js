05a5844bea3310eda2cba3e014c5c27e71f7be65
Connor Ferris <ferr0373@gmail.com>
2023-01-13 13:33:31 -0600

    completed addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,6 +3,22 @@ console.log('\n\n');
 
 let collection = [];
 
-function addTCollection(title, artist, yearPublished) {
-
+function addToCollection(title, artist, yearPublished) {
+   let album = { 
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+   };
+   collection.push(album);
+   return album;
 }
+
+
+console.log('Adding to collection', addToCollection ('Parachutes', 'Coldplay', 2000));
+console.log('Adding to collection', addToCollection ('Led Zeppelin IV', 'Led Zeppelin', 1971));
+console.log('Adding to collection', addToCollection ('Revolver', 'The Beatles', 1966));
+console.log('Adding to collection', addToCollection ('In Rainbows', 'Radiohead', 2007));
+console.log('Adding to collection', addToCollection ('They Want My Soul', 'Spoon', 2014));
+console.log('Adding to collection',addToCollection ('Halcyon Drift', 'C0N50', 2022));
+console.log('My album Collection is', collection);
+console.log('\n\n');
